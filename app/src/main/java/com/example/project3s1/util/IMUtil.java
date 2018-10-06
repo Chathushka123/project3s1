90eddb7175e7a1b5084684a87f664571c08f1289
Chathushka123 <cmchathushka123>
2018-10-06 07:16:15 +0530

    detect colour using filter curnel

--- app/src/main/java/com/example/project3s1/util/IMUtil.java
+++ app/src/main/java/com/example/project3s1/util/IMUtil.java
@@ -33,18 +33,12 @@ public class IMUtil
         rgb[1]=g/9;
         rgb[2]=b/9;
 
-<<<<<<< HEAD
-
         return rgb;
     }
 
 
-    public static int colourRound(int val) {
-=======
     public static int colourRound(int val)
     {
->>>>>>> fa706761e61c20ef2ecaa863bd3ff1e4e86f0a04
-
         if (val >= 0 && val <= 85) {
             val = 0;
         } else if (val >= 86 && val <170) {
@@ -56,48 +50,6 @@ public class IMUtil
 
         return val;
     }
-
-<<<<<<< HEAD
-
-  public static int hexValue(int[] rgb){
-
-      int r = colourRound(rgb[0]);
-      int g = colourRound(rgb[1]);
-      int b = colourRound(rgb[2]);
-      int hex;
-
-      hex = ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
-
-      return hex;
-  }
-
-  public static String findColour(int[] rgb){
-      String colour="null";
-
-      int hexValue=hexValue(rgb);
-
-      switch(hexValue){
-          case Colour.BLACK: return "BLACK";
-          case Colour.WHITE: return "WHITE";
-          case Colour.RED:   return "RED";
-          case Colour.LIME:  return "LIME";
-          case Colour.BLUE:  return "BLUE";
-          case Colour.YELLOW:return "YELLOW";
-          case Colour.CYAN:  return "CYAN";
-          case Colour.MAGENTA: return "MAGENTA";
-          case Colour.GRAY:  return "GRAY";
-          case Colour.MAROON:return "MAROON";
-          case Colour.OLIVE: return "OLIVE";
-          case Colour.GREEN: return "GREEN";
-          case Colour.PURPLE: return "PURPLE";
-          case Colour.TEAL:  return "TEAL";
-          case Colour.NAVY:  return "NAVY";
-
-      }
-
-      return colour;
-  }
-=======
     public static int hexValue(int[] rgb)
     {
         int r = colourRound(rgb[0]);
@@ -152,5 +104,4 @@ public class IMUtil
 
         return colour;
     }
->>>>>>> fa706761e61c20ef2ecaa863bd3ff1e4e86f0a04
 }
